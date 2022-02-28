3988e09c5d5ad1872967f44d94e22c0e583fbafb
Jose Wong <josemawo17@gmail.com>
2022-02-27 20:38:45 -0500

    produts-service(accounts&credits)

--- accounts-service/src/test/java/com/nttdata/proyect/accounts/AccountsServiceApplicationTests.java
+++ accounts-service/src/test/java/com/nttdata/proyect/accounts/AccountsServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.nttdata.proyect.bankproductsservice;
+package com.nttdata.proyect.accounts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BankProductsServiceApplicationTests {
+class AccountsServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
